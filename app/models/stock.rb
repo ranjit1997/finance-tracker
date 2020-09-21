1594648e47e5057fbe21fc92e8d08f1d0d73dfb2
ranjit1997 <rk1220245@gmail.com>
2020-09-21 17:53:39 +0530

    Add the stock_controller and show the price on browse

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,8 @@
 class Stock
 
   def self.new_lookup(ticker_symbol)
-    client = IEX::Api::Client.new(publishable_token: 'Tpk_37e8c10ec17c470aae9a7d98adb1cace',
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.config[:iex_client][:sendbox_api_key],
+
                                   endpoint: 'https://sandbox.iexapis.com/stable')
     client.price(ticker_symbol)                              
   end  
