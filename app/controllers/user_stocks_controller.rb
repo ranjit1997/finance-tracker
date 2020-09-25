37f9c84d6623258988f2bf3b339a9c641247d4ce
ranjit1997 <rk1220245@gmail.com>
2020-09-25 12:00:02 +0530

    add the user model

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,4 +1,5 @@
 class UserStocksController < ApplicationController
+
   def create
     stock = Stock.check_db(params[:ticker])
     if stock.blank?
@@ -8,5 +9,13 @@ class UserStocksController < ApplicationController
     @user_stock = UserStock.create(user: current_user, stock: stock)
     flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
     redirect_to my_portfolio_path  
-  end  
+  end
+  
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.ticker} was successfully removed from portfolio"
+    redirect_to my_portfolio_path
+  end
 end
